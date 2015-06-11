7acfe48d20dd190e822aae0d3dc6bcfa60a4d722
Tyler Funk <tfunk@trilixgroup.com>
2015-06-11 16:18:58 -0500

    Made each td into a directive

--- public/js/src/ng-calculator.js
+++ public/js/src/ng-calculator.js
@@ -4,8 +4,19 @@ angular.module('ngCalculator', [])
 
 .controller('NgCalculatorController', ['$scope', '$attrs',
   function($scope, $attrs) {
-    $scope.lol = 'rofl';
   }
+])
+
+.controller('NgButtonControler', ['$scope', '$attrs',
+    function($scope, $attrs) {
+        $scope.val = $attrs.calcButton;
+        $scope.colspan = $attrs.colspan ? $attrs.colspan : 1;
+        $scope.rowspan = $attrs.rowspan ? $attrs.rowspan : 1;
+
+        $scope.push = function() {
+            console.log($scope.val);
+        }
+    }
 ])
 
 .directive('ngCalculator', function() {
@@ -16,4 +27,15 @@ angular.module('ngCalculator', [])
     transclude: true,
     replace: true
   };
+})
+
+.directive('calcButton', function() {
+    return {
+        restrict: 'A',
+        controller: 'NgButtonControler',
+        templateUrl: 'js/template/button.html',
+        transclude: true,
+        replace: true,
+        scope: {}
+    };
 });
